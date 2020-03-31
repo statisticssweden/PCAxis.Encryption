0e9d7476f9ac922639405e787e7eb563a5914e8e
cklinton <60175636+cklinton@users.noreply.github.com>
2020-03-31 10:43:56 +0200

    A first commit

---
+++ PCAxis.Encryption.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PCAxis.Encryption.UnitTest
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
